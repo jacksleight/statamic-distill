f99534166fd2eb4e2e29d9411e97d67371de224e
Jack Sleight <jacksleight@gmail.com>
2023-03-06 09:32:19 +0000

    WIP

--- src/Distiller.php
+++ src/Distiller.php
@@ -2,17 +2,11 @@
 
 namespace JackSleight\StatamicDistill;
 
-use Statamic\Contracts\Assets\Asset;
-use Statamic\Contracts\Entries\Entry;
-use Statamic\Contracts\Taxonomies\Term;
 use Statamic\Fields\Value;
-use Statamic\Fields\Values;
-use Statamic\Fieldtypes\Bard;
-use Statamic\Structures\Page;
-use Statamic\Support\Arr;
+use Statamic\Query\IteratorBuilder;
 use Statamic\Support\Str;
 
-class Distiller
+class Distiller extends IteratorBuilder
 {
     const TYPE_ENTRY = 'entry';
 
@@ -42,355 +36,115 @@ class Distiller
 
     const FIELDTYPE_GRID = 'grid';
 
-    protected $value;
+    protected $walker;
 
-    protected $criteria = [];
+    protected $from;
 
-    protected $limit;
+    protected $type;
 
-    protected $found = [];
+    protected $path;
 
-    protected $result = [];
+    public function __construct()
+    {
+        $this->walker = new Walker($this);
+
+        return $this;
+    }
 
     public function from($value)
     {
-        $this->value = $value;
+        $this->from = $value;
 
         return $this;
     }
 
     public function type($type)
     {
-        $this->criteria[] = ['type', '__type', $type];
+        if (! is_array($type)) {
+            $type = explode('|', $type);
+        }
+
+        $regex = collect($type)
+            ->map(fn ($type) => ! Str::endsWith($type, '::*') ? "{$type}::*" : $type)
+            ->map(function ($type) {
+                return collect(preg_split('/(\*)/', $type, -1, PREG_SPLIT_DELIM_CAPTURE))
+                    ->map(fn ($part) => $part === '*' ? '[^\:]*' : preg_quote($part, '/'))
+                    ->join('');
+            })
+            ->join('|');
+
+        $this->type = "/^{$regex}/";
 
         return $this;
     }
 
-    public function where($name, $operator, $value)
+    public function path($path)
     {
-        $this->criteria[] = ["value.{$name}", $operator, $value];
+        if (! is_array($path)) {
+            $path = explode('|', $path);
+        }
+
+        $regex = collect($path)
+            ->map(function ($path) {
+                return collect(preg_split('/(\*)/', $path, -1, PREG_SPLIT_DELIM_CAPTURE))
+                    ->map(fn ($part) => $part === '*' ? '[^\.]*' : preg_quote($part, '/'))
+                    ->join('');
+            })
+            ->join('|');
+
+        $this->path = "/^{$regex}$/";
 
         return $this;
     }
 
-    public function limit($limit)
+    protected function getBaseItems()
     {
-        $this->limit = $limit;
+        $items = $this->walker->walk($this->from);
 
-        return $this;
+        return $this->collect($items);
     }
 
-    public function get()
+    protected function getFilteredItems()
     {
-        $this->found = [];
-        $this->result = [];
+        $items = $this->getBaseItems();
 
-        $this->collect($this->value);
-
-        return collect($this->result);
+        return $items;
     }
 
-    protected function collect($value, $path = ['root'], $type = [])
-    {
-        if ($value instanceof Page) {
-            $value = $value->entry();
-        }
-
-        if (! $type) {
-            if ($value instanceof Entry) {
-                array_push($type,
-                    self::TYPE_ENTRY,
-                    $value->collection()->handle(),
-                    $value->blueprint()->handle(),
-                );
-            } elseif ($value instanceof Term) {
-                array_push($type,
-                    self::TYPE_TERM,
-                    $value->taxonomy()->handle(),
-                );
-            } elseif ($value instanceof Asset) {
-                array_push($type,
-                    self::TYPE_ASSET,
-                    $value->container()->handle(),
-                    $value->mime_type,
-                );
-            } elseif ($value instanceof Value) {
-                array_push($type,
-                    self::TYPE_FIELD,
-                    optional($value->fieldtype())->handle()
-                );
-            }
-        } elseif (in_array($type[0], [self::TYPE_SET])) {
-            array_push($type,
-                $value['type']
-            );
-        } elseif (in_array($type[0], [self::TYPE_NODE, self::TYPE_MARK])) {
-            array_push($type,
-                $value['type']
-            );
-        }
-
-        $self = implode('.', $path);
-        $item = [
-            'type' => $type ? implode('::', $type) : null,
-            'path' => $self,
-            'name' => Arr::last($path),
-            'parent' => null,
-            'prev' => null,
-            'next' => null,
-            'value' => $value,
-        ];
-        $this->found[$self] = $item;
-        if ($this->checkCriteria($item)) {
-            if ($this->checkLimit()) {
-                $this->result[] = &$this->found[$self];
-            }
-            if ($path) {
-                $parent = implode('.', array_slice($path, 0, -1));
-                if (isset($this->found[$parent])) {
-                    $this->found[$self]['parent'] = &$this->found[$parent];
-                }
-            }
-            if ($path && in_array($type[0], [self::TYPE_SET, self::TYPE_ROW, self::TYPE_NODE, self::TYPE_MARK])) {
-                $prev = implode('.', array_merge(array_slice($path, 0, -1), [Arr::last($path) - 1]));
-                if (isset($this->found[$prev])) {
-                    $this->found[$self]['prev'] = &$this->found[$prev];
-                    $this->found[$prev]['next'] = &$this->found[$self];
-                }
-            }
-        }
-
-        $continue = $this->checkLimit(true);
-        if (! $continue) {
-            return false;
-        }
-
-        if ($type[0] === self::TYPE_FIELD) {
-            if ($type[1] === self::FIELDTYPE_ENTRIES) {
-                $continue = $this->collectEntries($value, $path);
-            } elseif ($type[1] === self::FIELDTYPE_TERMS) {
-                $continue = $this->collectTerms($value, $path);
-            } elseif ($type[1] === self::FIELDTYPE_ASSETS) {
-                $continue = $this->collectAssets($value, $path);
-            } elseif ($type[1] === self::FIELDTYPE_REPLICATOR) {
-                $continue = $this->collectReplicator($value, $path);
-            } elseif ($type[1] === self::FIELDTYPE_BARD) {
-                $continue = $this->collectBard($value, $path);
-            } elseif ($type[1] === self::FIELDTYPE_GRID) {
-                $continue = $this->collectGrid($value, $path);
-            }
-        } elseif ($type[0] === self::TYPE_ENTRY) {
-            $continue = $this->collectEntry($value, $path);
-        } elseif ($type[0] === self::TYPE_TERM) {
-            $continue = $this->collectTerm($value, $path);
-        } elseif ($type[0] === self::TYPE_ASSET) {
-            $continue = $this->collectAsset($value, $path);
-        } elseif ($type[0] === self::TYPE_SET) {
-            $continue = $this->collectSet($value, $path);
-        } elseif ($type[0] === self::TYPE_ROW) {
-            $continue = $this->collectRow($value, $path);
-        }
-
-        return $continue;
-    }
-
-    protected function checkCriteria($item)
-    {
-        foreach ($this->criteria as [$name, $operator, $checks]) {
-            $value = data_get($item, $name);
-            if ($operator === '=') {
-                if ($value !== $checks) {
-                    return false;
-                }
-            } elseif ($operator === '__type') {
-                $checks = collect(explode('|', $checks))
-                    ->map(fn ($type) => ! Str::endsWith($type, '::*') ? "{$type}::*" : $type)
-                    ->all();
-                foreach ($checks as $check) {
-                    if (Str::is($check, "{$value}::")) {
-                        continue 2;
-                    }
-                }
-
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    protected function checkLimit($offset = false)
+    public function shouldContinue($overrun = false)
     {
         if (! isset($this->limit)) {
             return true;
         }
 
-        return count($this->result) < ($this->limit + ($offset ? 1 : 0));
+        $limit = $this->offset + $this->limit;
+
+        if ($overrun) {
+            $limit++;
+        }
+
+        return count($this->walker->items) < $limit;
     }
 
-    protected function collectEntries(Value $value, $path)
+    public function shouldInclude($item)
     {
+        if ($this->type && ! preg_match($this->type, $item['type'])) {
+            return false;
+        }
+
+        if ($this->path && ! preg_match($this->path, $item['path'])) {
+            return false;
+        }
+
+        if ($this->wheres) {
+            if ($item['value'] instanceof Value) {
+                return false;
+            }
+            if (! $this->filterWheres(collect([$item['value']]))->count()) {
+                return false;
+            }
+        }
+
         return true;
-    }
-
-    protected function collectEntry(Entry $entry, $path)
-    {
-        $stack = $entry->blueprint()->fields()->all()->keys()->all();
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $name = array_shift($stack);
-            $value = $entry->augmentedValue($name);
-            $continue = $this->collect($value, array_merge($path, [$name]));
-        }
-
-        return $continue;
-    }
-
-    protected function collectTerms(Value $value, $path)
-    {
-        return true;
-    }
-
-    protected function collectTerm(Entry $term, $path)
-    {
-        return true;
-    }
-
-    protected function collectAssets(Value $value, $path)
-    {
-        $data = Arr::wrap($value->raw()) ?? [];
-        $stack = array_keys($data);
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $index = array_shift($stack);
-            $item = $data[$index];
-            $asset = $value->fieldtype()->augment($item);
-            $continue = $this->collect($asset, array_merge($path, [$index]));
-        }
-
-        return $continue;
-    }
-
-    protected function collectAsset(Asset $asset, $path)
-    {
-        return true;
-    }
-
-    protected function collectReplicator(Value $value, $path)
-    {
-        $data = $value->raw() ?? [];
-        $stack = array_keys($data);
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $index = array_shift($stack);
-            $item = $data[$index];
-            $set = $value->fieldtype()->augment([$item])[0];
-            $continue = $this->collect($set, array_merge($path, [$index]), [self::TYPE_SET]);
-        }
-
-        return $continue;
-    }
-
-    protected function collectBard(Value $value, $path)
-    {
-        return $this->collectBardNodes($value->raw() ?? [], $path, $value->fieldtype());
-    }
-
-    protected function collectBardNodes($nodes, $path, Bard $fieldtype)
-    {
-        $stack = array_keys($nodes);
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $index = array_shift($stack);
-            $node = $nodes[$index];
-            $continue = $node['type'] === 'set'
-                ? $this->collectBardSet($node, array_merge($path, [$index]), $fieldtype)
-                : $this->collectBardNode($node, array_merge($path, [$index]), $fieldtype);
-        }
-
-        return $continue;
-    }
-
-    protected function collectBardSet($set, $path, Bard $fieldtype)
-    {
-        $set = $fieldtype->augment([$set])[0];
-
-        return $this->collect($set, $path, [self::TYPE_SET]);
-    }
-
-    protected function collectBardNode($item, $path, Bard $fieldtype)
-    {
-        $continue = $this->collect($item, $path, [self::TYPE_NODE]);
-
-        if ($continue) {
-            $continue = $this->collectBardMarks($item['marks'] ?? [], $path, $fieldtype);
-        }
-        if ($continue) {
-            $continue = $this->collectBardNodes($item['content'] ?? [], $path, $fieldtype);
-        }
-
-        return $continue;
-    }
-
-    protected function collectBardMarks($marks, $path)
-    {
-        $stack = array_keys($marks);
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $index = array_shift($stack);
-            $mark = $marks[$index];
-            $continue = $this->collect($mark, array_merge($path, [$index]), [self::TYPE_MARK]);
-        }
-
-        return $continue;
-    }
-
-    protected function collectGrid(Value $value, $path)
-    {
-        $data = $value->raw() ?? [];
-        $stack = array_keys($data);
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $index = array_shift($stack);
-            $item = $data[$index];
-            $row = $value->fieldtype()->augment([$item])[0];
-            $continue = $this->collect($row, array_merge($path, [$index]), [self::TYPE_ROW]);
-        }
-
-        return $continue;
-    }
-
-    protected function collectSet(Values $set, $path)
-    {
-        $stack = $set->getProxiedInstance()->except(['id', 'type', 'enabled'])->keys()->all();
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $name = array_shift($stack);
-            $value = $set->getProxiedInstance()->get($name);
-            $continue = $this->collect($value, array_merge($path, [$name]));
-        }
-
-        return $continue;
-    }
-
-    protected function collectRow(Values $row, $path)
-    {
-        $stack = $row->getProxiedInstance()->except(['id'])->keys()->all();
-
-        $continue = true;
-        while ($continue && count($stack)) {
-            $name = array_shift($stack);
-            $value = $row->getProxiedInstance()->get($name);
-            $continue = $this->collect($value, array_merge($path, [$name]));
-        }
-
-        return $continue;
     }
 }
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,10 +12,9 @@ class ServiceProvider extends AddonServiceProvider
 function distill($value)
 {
     $data = Distill::from($value)
-        ->type('asset')
-        ->limit(1)
-        ->get()
-        ->first();
+        ->offset(1)
+        ->limit(3)
+        ->get();
 
     dd($data);
 }
